7cd10893985389485a687a0c2add133d220307bf
SoN41 <100400600+SoN41@users.noreply.github.com>
2024-03-26 16:15:38 +0530

    frontend changes

---
+++ frontend/src/components/sidebar/LogoutBtn.jsx
@@ -0,0 +1,21 @@
+import {BiLogOut} from 'react-icons/bi'
+import useLogout from '../../hooks/useLogout';
+
+const LogoutBtn = () => {
+
+  const {loading , logout} = useLogout();
+
+  return (
+    <div className=" mt-auto">
+        {!loading ? (
+          <BiLogOut className=' w-6 h-6 text-white cursor-pointer'
+          onClick={logout}
+        />
+        ) : (
+          <span className='loading loading-spiner' ></span>
+        )}
+    </div>
+  )
+}
+
+export default LogoutBtn;
---
+++ frontend/src/pages/signup/GenderBox.jsx
@@ -0,0 +1,28 @@
+const GenderBox = ({onCheckboxChange , selectedGender}) => {
+  return (
+    <div>
+        <div className="flex">
+            <div className="form-control">
+                <label className={`label gap-2 cursor-pointer ${selectedGender === 'male' ? 'selected': ''} `}>
+                    <span className="label-tex text-blue-500">Male</span>
+                    <input type="checkbox" className=" checkbox border-slate-900" 
+                        checked={selectedGender == 'male'}
+                        onChange={() => onCheckboxChange("male")}
+                    />
+                </label>
+            </div>
+            <div className="form-control">
+                <label className={`label gap-2 cursor-pointer ${selectedGender === 'female' ? 'selected': ''}`}>
+                    <span className="label-text text-blue-500">Female</span>
+                    <input type="checkbox" className=" checkbox border-slate-900" 
+                        checked={selectedGender == 'female'}
+                        onChange={() => onCheckboxChange("female")}
+                    />
+                </label>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default GenderBox
