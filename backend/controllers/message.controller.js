41c391dbf3a9b4057b9c5e8713ff554777370228
SoN41 <100400600+SoN41@users.noreply.github.com>
2024-03-26 16:11:52 +0530

    backend changes

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -1,5 +1,6 @@
 import Conversation from "../models/conversation.model.js";
 import Message from "../models/message.model.js";
+import { getReceiverSocketId, io } from "../socket/socket.js";
 
 export const sendMessage = async(req,res) => {
     try {
@@ -29,6 +30,12 @@ export const sendMessage = async(req,res) => {
         
         // This will run perallel
         await Promise.all([conversation.save() , newMessage.save()]);
+
+        const receiverSocketId = getReceiverSocketId(reciverId);
+        if(receiverSocketId) {
+            io.to(receiverSocketId).emit("newMessage" , newMessage)  // used to send events to specific client
+        }
+
         res.status(201).json(newMessage)
         
     } catch (e) {
